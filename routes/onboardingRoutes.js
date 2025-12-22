02a70586dd00efb99bcc72f01f6912584fb060cc
henry wu <wuhenry72@gmail.com>
2025-12-22 18:54:59 -0500

    all in one commit
    things are done

---
+++ routes/onboardingRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../middlewares/upload');
+const onboardingController = require('../controllers/onboardingController');
+const protect = require('../middlewares/auth.js'); 
+
+router.get('/:username', protect, onboardingController.getOnboardingData);
+
+router.patch('/submit', protect,
+    upload.fields([
+        { name: 'profilePicture', maxCount: 1 },
+        { name: 'licenseCopy', maxCount: 1 },
+        { name: 'optReceipt', maxCount: 1 }
+    ]), 
+    onboardingController.submitOnboarding
+);
+
+module.exports = router;
