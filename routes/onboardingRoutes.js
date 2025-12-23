8c8ae46b5cbbd8fe3f0da83e45740864c19b7b13
henry wu <wuhenry72@gmail.com>
2025-12-22 19:08:43 -0500

    Update onboardingRoutes.js
    final

--- routes/onboardingRoutes.js
+++ routes/onboardingRoutes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../middlewares/upload');
 const onboardingController = require('../controllers/onboardingController');
-const protect = require('../middlewares/auth.js'); 
+const protect = require('../middlewares/auth.js');
 
 router.get('/:username', protect, onboardingController.getOnboardingData);
 
-router.patch('/submit', protect,
-    upload.fields([
+router.patch('/submit', upload.fields([
         { name: 'profilePicture', maxCount: 1 },
         { name: 'licenseCopy', maxCount: 1 },
         { name: 'optReceipt', maxCount: 1 }
-    ]), 
+    ]),
+    protect,
     onboardingController.submitOnboarding
 );
 
